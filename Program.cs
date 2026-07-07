854c1414e8cf2e6f6ea890ef15bc5ccb63436221
black <black@example.com>
2026-07-07 23:16:57 +0500

    Change greeting to Hello everyone!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello everyone!");
+void greetWhite()
+{
+    Console.WriteLine("Hello White");
+}
